672b7848d9f61f8e3920953f3656138b492d0014
gwpark <pawoo927@naver.com>
2022-04-07 11:16:11 +0900

    merge

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -3,11 +3,8 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		System.out.println("Add java file");
-<<<<<<< HEAD
 		System.out.println("hello in local");
-=======
 		System.out.println("hello...");
->>>>>>> refs/remotes/origin/master
 
 	}
 
